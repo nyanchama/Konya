0c34803cabcb6b7f22c245f4f12aac78ed82e4e7
Nyanchama Obara <92072653+nyanchama@users.noreply.github.com>
2025-12-09 04:28:13 +0300

    Update mybeaut.js
    css again

--- src/mybeaut.js
+++ src/mybeaut.js
@@ -88,7 +88,7 @@ btnYes.addEventListener("click", () => {
   staticShort.play();
 
    
-  tvMessage.textContent = "baby, here's a small place i dreamed up for you 👇🏾";
+  tvMessage.textContent = "baby, here's a small place i dreamed up for you ";
   tvBed.classList.remove("hidden");
 
 });
